59c75b33887a8e18e1ba87417876f2bcd9c2e732
shubham <shubham@shubhams-MacBook-Air.local>
2024-05-13 11:55:38 +0530

    msg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import SignUp from "./components/Auth/SignUp";
 import EmailVerify from "./components/EmailVerify/EmailVerify";
+import Services from "./components/Services/Services";
+import HotelCard from "./components/Cards/HotelCard";
+import LandingPage from "./components/LandingPage copy/LandingPage";
 
 
 
@@ -20,6 +23,10 @@ function App() {
 
           <Route path='/login' element={<Login /> } />
           <Route path='/register' element={<SignUp /> } />
+          <Route path='/service' element={<Services /> } />
+          <Route path='/hotel' element={<HotelCard /> } />
+          <Route path='/landing' element={<LandingPage /> } />
+
           <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
 
 
